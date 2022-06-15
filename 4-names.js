4c46531d7974ae7823204ad55820b10b3468c289
Mohammed Zayyam <zayya002@umn.edu>
2022-06-15 00:33:16 -0500

    first commit

---
+++ 4-names.js
@@ -0,0 +1,6 @@
+const john = 'john';
+const susan = 'susan';
+
+console.log(module)
+
+module.exports = {john, susan}
---
+++ 8-module.js
@@ -0,0 +1,10 @@
+//Modules
+
+const names = require('./4-names');
+const data = require('./6-alternative-flavours');
+const sayHi = require('./5-utils');
+require('./7-mid-grenade');
+//console.log(names);
+///ayHi(names.john);
+//sayHi(names.susan);
+//console.log(data);
